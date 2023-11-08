7a92ecd7ebf0b9202dd6099f9b286243b1cf6902
pc <pc@BATSEBA>
2023-11-07 21:48:31 -0600

    Agregar archivos de proyecto.

---
+++ Parcial2BB/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.cboDepartamento = New System.Windows.Forms.ComboBox()
+        Me.cboMunicipio = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(337, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(99, 31)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cliente"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(282, 95)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(197, 20)
+        Me.txtNombre.TabIndex = 1
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(282, 148)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(197, 20)
+        Me.txtApellido.TabIndex = 2
+        '
+        'cboDepartamento
+        '
+        Me.cboDepartamento.FormattingEnabled = True
+        Me.cboDepartamento.Location = New System.Drawing.Point(282, 208)
+        Me.cboDepartamento.Name = "cboDepartamento"
+        Me.cboDepartamento.Size = New System.Drawing.Size(196, 21)
+        Me.cboDepartamento.TabIndex = 3
+        '
+        'cboMunicipio
+        '
+        Me.cboMunicipio.FormattingEnabled = True
+        Me.cboMunicipio.Location = New System.Drawing.Point(282, 264)
+        Me.cboMunicipio.Name = "cboMunicipio"
+        Me.cboMunicipio.Size = New System.Drawing.Size(195, 21)
+        Me.cboMunicipio.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(282, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(282, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(44, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Apellido"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(286, 186)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(74, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Departamento"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(282, 245)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Municipio"
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(343, 329)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(75, 23)
+        Me.btnGuardar.TabIndex = 9
+        Me.btnGuardar.Text = "Insertar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cboMunicipio)
+        Me.Controls.Add(Me.cboDepartamento)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents cboDepartamento As ComboBox
+    Friend WithEvents cboMunicipio As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnGuardar As Button
+End Class
---
+++ Parcial2BB/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim connectionString As String = "Data Source=BATSEBA\SQLEXPRESS01;Initial Catalog=BDParcial2BrendayBatseba;Integrated Security=True;"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CargarDepartamentos()
+        CargarMunicipios()
+    End Sub
+
+    Private Sub CargarDepartamentos()
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+
+            Dim query As String = "SELECT Departamentoid, Nombre FROM DEPARTAMENTO"
+            Dim adapter As New SqlDataAdapter(query, connection)
+            Dim dataSet As New DataSet()
+
+            adapter.Fill(dataSet, "DEPARTAMENTO")
+
+            cboDepartamento.DataSource = dataSet.Tables("DEPARTAMENTO")
+            cboDepartamento.DisplayMember = "Nombre"
+            cboDepartamento.ValueMember = "Departamentoid"
+        End Using
+    End Sub
+
+    Private Sub CargarMunicipios()
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+
+            Dim query As String = "SELECT Municipioid, Nombre FROM MUNICIPIO"
+            Dim adapter As New SqlDataAdapter(query, connection)
+            Dim dataSet As New DataSet()
+
+            adapter.Fill(dataSet, "MUNICIPIO")
+
+            cboMunicipio.DataSource = dataSet.Tables("MUNICIPIO")
+            cboMunicipio.DisplayMember = "Nombre"
+            cboMunicipio.ValueMember = "Municipioid"
+        End Using
+    End Sub
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Using connection As New SqlConnection(connectionString)
+            connection.Open()
+
+            Dim query As String = "INSERT INTO CLIENTE (Nombre, Apellido, Departamentoid, Municipioid) VALUES (@Nombre, @Apellido, @Departamentoid, @Municipioid)"
+
+            Using command As New SqlCommand(query, connection)
+                command.Parameters.AddWithValue("@Nombre", txtNombre.Text)
+                command.Parameters.AddWithValue("@Apellido", txtApellido.Text)
+                command.Parameters.AddWithValue("@Departamentoid", cboDepartamento.SelectedValue)
+                command.Parameters.AddWithValue("@Municipioid", cboMunicipio.SelectedValue)
+
+                command.ExecuteNonQuery()
+            End Using
+        End Using
+
+        ' Muestra un mensaje de éxito.
+        MessageBox.Show("Los datos se han insertado con éxito.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
